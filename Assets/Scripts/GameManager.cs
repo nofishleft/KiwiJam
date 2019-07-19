bfa6c4e0c072ff2b305af1e83d544712f01c3fa1
NeedsSoySauce <feras.albaroudi@gmail.com>
2019-07-20 01:18:57 +1200

    Added GameManager with scene changing methods

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+
+    public string mainMenuSceneName = "MainMenu";
+
+    [Tooltip("First level at index 0")]
+    public string[] sceneNames;
+
+    string currentSceneName;
+    int sceneIndex = 0;
+
+    // The following is kind've crappy way to test this, but it works!
+    public bool triggerMainMenuScene = false;
+    public bool triggerRestartScene = false;
+    public bool triggerNextScene = false; 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // This is only implemented for testing, it can be removed later
+    private void Update()
+    {
+        if (triggerMainMenuScene)
+        {
+            loadMainMenu();
+        }
+        else if (triggerRestartScene)
+        {
+            restartScene();
+        }
+        else if (triggerNextScene)
+        {
+            nextScene();
+        }
+
+        triggerMainMenuScene = false;
+        triggerRestartScene = false;
+        triggerNextScene = false;
+    }
+
+    public void loadMainMenu()
+    {
+        StartCoroutine(LoadSceneInBackground(mainMenuSceneName));
+        currentSceneName = mainMenuSceneName;
+        sceneIndex = 0;
+    }
+
+    public void restartScene()
+    {
+        StartCoroutine(LoadSceneInBackground(currentSceneName));
+    }
+
+    public void nextScene()
+    {
+        // Heavily based on https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadSceneAsync.html
+        string sceneName = sceneNames[sceneIndex];
+        StartCoroutine(LoadSceneInBackground(sceneName));
+        sceneIndex = (sceneIndex + 1) % sceneNames.Length;
+        currentSceneName = sceneName;
+    }
+
+    IEnumerator LoadSceneInBackground(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
