ce0e9290c11aa3e1575776c0aad70f901f8157d9
Unknown <feras.albaroudi@gmail.com>
2019-07-21 10:58:52 +1200

    Timer Initial Delay

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
 
+    public int initialDelay = 15;
+    public static int initialDelayStatic;
     public int durationSeconds = 90;
     TextMeshProUGUI text;
     GameManager gm;
@@ -21,13 +23,13 @@ public class Timer : MonoBehaviour
 
     public void StartTimer()
     {
-        StartCoroutine(Countdown());
+        StartCoroutine(initialDelayCountdown());
     }
 
-    string getMinutesSeconds()
+    string getMinutesSeconds(int time)
     {
-        int min = Mathf.FloorToInt(durationSeconds / 60);
-        int sec = durationSeconds - (min * 60);
+        int min = Mathf.FloorToInt(time / 60);
+        int sec = time - (min * 60);
         string secStr = sec.ToString();
         if (sec < 10)
         {
@@ -36,11 +38,24 @@ public class Timer : MonoBehaviour
         return min.ToString() + ':' + secStr.ToString();
     }
 
+    IEnumerator initialDelayCountdown()
+    {
+        while (initialDelay > 0)
+        {
+            text.text = getMinutesSeconds(initialDelay);
+            yield return new WaitForSeconds(1.0f);
+            initialDelay--;
+        }
+
+        StartCoroutine(Countdown());
+
+    }
+
     IEnumerator Countdown()
     {
         while (durationSeconds > 0)
         {
-            text.text = getMinutesSeconds();
+            text.text = getMinutesSeconds(durationSeconds);
             yield return new WaitForSeconds(1.0f);
             durationSeconds--;
         }
