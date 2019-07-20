dc8848bb2e22f4201093cc813be7ba59d1397e29
Unknown <feras.albaroudi@gmail.com>
2019-07-21 11:46:41 +1200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/SwapTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapTimer : MonoBehaviour
+{
+    Image filled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filled = GetComponent<Image>();
+        StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        while (true)
+        {
+            float percent = TileController.timeStatic / TileController.timeStaticMax;
+
+            filled.fillAmount = percent;
+            yield return null;
+        }
+    }
+}
